2c14d291566ac25c4c28b1af1f26207d5d640362
pengfei <pengfeiye1994@gmail.com>
2018-03-04 02:11:37 -0800

    test

--- routes/stream.js
+++ routes/stream.js
@@ -7,26 +7,18 @@ var shell = require('shelljs');
 var request = require('request');
 
 
-router.get('/', function(req,res){
+router.get('/', function(req){
   var query = req.query.hlsurl
   var dir = request(query).uri.pathname.split('/')
   shell.mkdir('-p', 'public/stream/'+dir[1])
   request(query).pipe(fs.createWriteStream('./public/stream/'+dir[1]+'/'+dir[2]))
-
-  http.createServer(function(request, response) {
-    var filePath = './public/stream/'+dir[1]+'/'+dir[2];
-    var stat = fs.statSync(filePath);
-
-    response.writeHead(200, {
-        'Content-Type': 'm3u8',
-        'Content-Length': stat.size
-    });
-
-    var readStream = fs.createReadStream(filePath);
-    // We replaced all the event handlers with a simple call to readStream.pipe()
-    readStream.pipe(response);
+  var server = http.createServer(function(request,res){
+    fs.readFile(__dirname + '/stream/'+dir[1]+'/'+dir[2], function(err,data){
+      res.end(data)
+    })
   })
-  .listen(2000);
+
+  server.listen(8000)
 });
 
 module.exports = router;
