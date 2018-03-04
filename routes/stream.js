5bc7d9006e48c5b2a5edb0ae4a02fa63c58ece1e
pengfei <pengfeiye1994@gmail.com>
2018-03-04 12:30:06 -0800

    test

--- routes/stream.js
+++ routes/stream.js
@@ -5,20 +5,26 @@ var fs = require('fs');
 var url = require('url');
 var shell = require('shelljs');
 var request = require('request');
+var m3u8 = require('m3u8');
 
+const parser = m3u8.createStream();
 
-router.get('/', function(req){
+router.get('/', function(req, res){
   var query = req.query.hlsurl
   var dir = request(query).uri.pathname.split('/')
   shell.mkdir('-p', 'public/stream/'+dir[1])
   request(query).pipe(fs.createWriteStream('./public/stream/'+dir[1]+'/'+dir[2]))
-  var server = http.createServer(function(request,res){
-    fs.readFile(__dirname + '/stream/'+dir[1]+'/'+dir[2], function(err,data){
-      res.end(data)
-    })
-  })
+  // var server = http.createServer(function(request,res){
+  //   fs.readFile(__dirname + '/stream/'+dir[1]+'/'+dir[2], function(err,data){
+  //     res.end(data)
+  //   })
+  // })
+  //
+  // server.listen(8000)
+  console.log()
+  var x = fs.createReadStream('./public/stream.m3u8')
 
-  server.listen(8000)
+  res.json(x.pipe(parser))
 });
 
 module.exports = router;
