463c47f33550a446bc1767bc3c88acd87bee70bb
pengfei <pengfeiye1994@gmail.com>
2018-03-04 14:46:14 -0800

    test

--- routes/stream.js
+++ routes/stream.js
@@ -22,11 +22,38 @@ router.get('/', function(req, res){
   //
   // server.listen(8000)
 
-  var video = fs.createReadStream('./public/stream/'+dir[1]+'/'+dir[2])
+  var video = fs.statSync('./public/stream/'+dir[1]+'/'+dir[2])
   var video2 = fs.createReadStream('./public/test.txt')
-  video.pipe(parser)
+  var path = './public/stream/'+dir[1]+'/'+dir[2]
+  var range = req.headers.range
+  var fileSize = video.size
 
-  res.download('./public/stream/'+dir[1]+'/'+dir[2])
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'hls',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'hls',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
 
 
 
