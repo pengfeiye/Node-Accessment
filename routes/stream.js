a2ae0653110623e2a6b527aa5b08e510ce21edae
pengfei <pengfeiye1994@gmail.com>
2018-03-04 01:41:17 -0800

    test

--- routes/stream.js
+++ routes/stream.js
@@ -12,7 +12,21 @@ router.get('/', function(req,res){
   var dir = request(query).uri.pathname.split('/')
   shell.mkdir('-p', 'public/stream/'+dir[1])
   request(query).pipe(fs.createWriteStream('./public/stream/'+dir[1]+'/'+dir[2]))
-  res.json(fs.createReadStream('./public/stream/'+dir[1]+'/'+dir[2]))
+
+  http.createServer(function(request, response) {
+    var filePath = './public/stream/'+dir[1]+'/'+dir[2];
+    var stat = fs.statSync(filePath);
+
+    response.writeHead(200, {
+        'Content-Type': 'm3u8',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fs.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(response);
+  })
+  .listen(2000);
 });
 
 module.exports = router;
