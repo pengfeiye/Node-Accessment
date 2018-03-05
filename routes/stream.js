565e7e61118145e938c4c5916d68e92a23d4ceee
pengfei <pengfeiye1994@gmail.com>
2018-03-04 21:27:19 -0800

    asd

--- routes/stream.js
+++ routes/stream.js
@@ -6,57 +6,29 @@ var url = require('url');
 var shell = require('shelljs');
 var request = require('request');
 var m3u8 = require('m3u8');
+var HLSServer = require('hls-server')
 
 var parser = m3u8.createStream();
 
-router.get('/', function(req, res){
+http.createServer(router.get('/', function(req, res){
   var query = req.query.hlsurl
   var dir = request(query).uri.pathname.split('/')
   shell.mkdir('-p', 'public/stream/'+dir[1])
   request(query).pipe(fs.createWriteStream('./public/stream/'+dir[1]+'/'+dir[2]))
-  // var server = http.createServer(function(request,res){
-  //   fs.readFile(__dirname + '/stream/'+dir[1]+'/'+dir[2], function(err,data){
-  //     res.end(data)
-  //   })
+  // http.createServer(function(request,response){
+  //   response.writeHead(200,{'Content-Type':'video/m3u8'});
+  //   fs.createReadStream('./public/stream/'+dir[1]+'/'+dir[2]).pipe(response)
+  // }).listen(8000)
+  fs.createReadStream('./public/stream/'+dir[1]+'/'+dir[2]).pipe(res)
+
+
+
+  // var server = http.createServer()
+  // var hls = new HLSServer(server, {
+  //   path: '/stream'+query,     // Base URI to output HLS streams
+  //   dir: path  // Directory that input files are stored
   // })
-  //
-  // server.listen(8000)
 
-  var video = fs.statSync('./public/stream/'+dir[1]+'/'+dir[2])
-  var video2 = fs.createReadStream('./public/test.txt')
-  var path = './public/stream/'+dir[1]+'/'+dir[2]
-  var range = req.headers.range
-  var fileSize = video.size
-
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1]
-      ? parseInt(parts[1], 10)
-      : fileSize-1
-
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'hls',
-    }
-
-    res.writeHead(206, head)
-    file.pipe(res)
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'hls',
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
-  }
-
-
-
-});
+})).listen(8000)
 
 module.exports = router;
